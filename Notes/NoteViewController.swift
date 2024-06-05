f246b14fe3bf792db0090c2a89e7260a91104a5b
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-06-05 16:24:49 +0900

    Initial commit

---
+++ Notes/NoteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NoteViewController.swift
+//  Notes
+//
+//  Created by chj on 6/5/24.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var noteLabel: UITextView!
+    
+    public var noteTitle: String = ""
+    public var note: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = noteTitle
+        noteLabel.text = note
+    }
+    
+}
